3e97bd1b71ca54b8f787a47ffec6e319bc31f2da
Thang Tran <tuthang102@gmail.com>
2017-11-01 15:41:35 -0500

    Delete largest_label_volume.m

--- largest_label_volume.m
+++
@@ -1,9 +0,0 @@
-function [vals, counts]= largest_label_volume(im,bg=-1)
-
-[vals counts]=unique(im);
-counts=counts(vals ~= bg);
-vals= vals( vals~= bg);
-
-if length(counts)>0
-   vals(max(counts))
-end
